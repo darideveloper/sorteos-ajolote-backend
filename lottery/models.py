44aa415fd287e0e0e3dc5e83861bdfdffa371bf8
darideveloper <darideveloper@gmail.com>
2023-05-18 04:56:23 -0600

    feat (models/Lottery): optional fields

--- lottery/models.py
+++ lottery/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Lottery (models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, verbose_name="nombre", help_text="Nombre del sorteo", unique=True)
-    details = models.TextField(verbose_name="detalles", help_text="Detalles del sorteo")
+    details = models.TextField(verbose_name="detalles", help_text="Detalles del sorteo", null=True, blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio total", help_text="Precio total del sorteo (el costo de los boletos será: precio total / cantidad de boletos)")
-    image = models.URLField (verbose_name="imagen", help_text="URL de la imagen del sorteo")
-    end_date = models.DateTimeField(verbose_name="fecha de finalización", help_text="Fecha de finalización del sorteo")
+    image = models.URLField (verbose_name="imagen", help_text="URL de la imagen del sorteo", null=True, blank=True)
+    end_date = models.DateTimeField(verbose_name="fecha de finalización", help_text="Fecha de finalización del sorteo", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación", help_text="Fecha de creación del sorteo", editable=False)
     numbers = models.IntegerField(verbose_name="cantidad de números", help_text="Cantidad de números que se pueden elegir en el sorteo")
     is_open = models.BooleanField(default=True, verbose_name="abierto", help_text="Indica si el sorteo está abierto")
