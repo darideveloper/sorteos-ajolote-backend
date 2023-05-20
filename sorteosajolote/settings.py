63d6f408a0abbbe37b79aed3606efb2abe908d70
darideveloper <darideveloper@gmail.com>
2023-05-19 23:04:42 -0600

    feat (settings): save images in aws s3

--- sorteosajolote/settings.py
+++ sorteosajolote/settings.py
@@ -30,6 +30,7 @@ DEBUG = os.environ.get("DEBUG") == "True"
 INSTALLED_APPS = [
     'lottery',
     'corsheaders',
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -139,11 +140,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 
-# Activate whitenoise only in production
-if DEBUG:
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-else:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
@@ -153,11 +151,26 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://sorteos-ajolote.darideveloper.com",
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
-
 CORS_ALLOW_ALL_ORIGINS = True
 
+# aws settings
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# s3 static settings
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'sorteosajolote.storage_backends.StaticStorage'
+# s3 public media settings
+
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'sorteosajolote.storage_backends.PublicMediaStorage'
+
+# s3 private media settings
+PRIVATE_MEDIA_LOCATION = 'private'
+PRIVATE_FILE_STORAGE = 'sorteosajolote.storage_backends.PrivateMediaStorage'
